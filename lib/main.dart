14c664928b2cc4c4eaa5e742ba69ae984d17a39d
quarshie133 <wrightsprince@gmail.com>
2024-03-29 13:17:45 +0000

    to do app layout backgroundcolor

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
